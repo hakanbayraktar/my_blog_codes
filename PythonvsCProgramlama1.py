921890c99f74e0cd2c2b31c51180d294cc8461f3
blalyasar <49458946+blalyasar@users.noreply.github.com>
2020-05-25 05:12:04 +0300

    Create PythonvsCProgramlama1.py

---
+++ PythonvsCProgramlama1.py
@@ -0,0 +1,39 @@
+
+"""
+    )   (           (                                   
+ ( /(   )\      )   )\   (         )           )   (    
+ )\()) ((_)  ( /(  ((_)  )\ )   ( /(   (    ( /(   )(   
+((_)\   _    )(_))  _   (()/(   )(_))  )\   )(_)) (()\  
+| |(_) | |  ((_)_  | |   )(_)) ((_)_  ((_) ((_)_   ((_) 
+| '_ \ | |  / _` | | |  | || | / _` | (_-< / _` | | '_| 
+|_.__/ |_|  \__,_| |_|   \_, | \__,_| /__/ \__,_| |_|   
+                         |__/                           
+
+"""
+
+
+print("HELLO WORLD")# HELLO WORLD
+
+
+#include<stdio.h>
+main()
+{
+printf("HELLO WORLD");
+}// HELLO WORLD
+
+
+
+
+a = "Merhaba Medium Okuyucusu"
+print(a)# Merhaba Medium Okuyucusu
+
+
+
+#include<stdio.h>
+main()
+{
+ char isim[] = "Merhaba Medium Okuyucusu";
+ printf(isim);
+}// Merhaba Medium Okuyucusu
+
+
