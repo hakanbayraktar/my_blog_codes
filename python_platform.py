0d17084abb68cd6a08f8a1b47a63ff98e4051e94
blalyasar <49458946+blalyasar@users.noreply.github.com>
2020-05-25 05:02:29 +0300

    Create python_platform.py

---
+++ python_platform.py
@@ -0,0 +1,18 @@
+
+"""
+    )   (           (                                   
+ ( /(   )\      )   )\   (         )           )   (    
+ )\()) ((_)  ( /(  ((_)  )\ )   ( /(   (    ( /(   )(   
+((_)\   _    )(_))  _   (()/(   )(_))  )\   )(_)) (()\  
+| |(_) | |  ((_)_  | |   )(_)) ((_)_  ((_) ((_)_   ((_) 
+| '_ \ | |  / _` | | |  | || | / _` | (_-< / _` | | '_| 
+|_.__/ |_|  \__,_| |_|   \_, | \__,_| /__/ \__,_| |_|   
+                         |__/                           
+
+"""
+
+import platform# Mimari
+print("Mimari: " + platform.architecture()[0])# machine
+print("Machine: " + platform.machine())# node
+print("Node: " + platform.node())# system
+print("System: " + platform.system())
