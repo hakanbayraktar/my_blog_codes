1b59269efdce97e27cd13259cec25edc06d9f98e
blalyasar <49458946+blalyasar@users.noreply.github.com>
2020-05-25 05:28:44 +0300

    Create PythonDosyaİslemleri.py

---
+++ PythonDosyaİslemleri.py
@@ -0,0 +1,22 @@
+
+"""
+    )   (           (                                   
+ ( /(   )\      )   )\   (         )           )   (    
+ )\()) ((_)  ( /(  ((_)  )\ )   ( /(   (    ( /(   )(   
+((_)\   _    )(_))  _   (()/(   )(_))  )\   )(_)) (()\  
+| |(_) | |  ((_)_  | |   )(_)) ((_)_  ((_) ((_)_   ((_) 
+| '_ \ | |  / _` | | |  | || | / _` | (_-< / _` | | '_| 
+|_.__/ |_|  \__,_| |_|   \_, | \__,_| /__/ \__,_| |_|   
+                         |__/                           
+
+"""
+
+import osmetindosyalari = [] #txt dosyalarını yazacağımız dizi
+os.chdir(‘….’) #dosya yolu
+for txtdosyam in os.listdir(os.getcwd()): #dosya yolu ıcındekı dosyalar    
+    if txtdosyam.endswith(‘.txt’): #sonu txt ile biten dosyalar        
+        #print(txtdosyam)        
+        metindosyalari.append(txtdosyam)#metindosyalaridizisine
+ekleme
+#print(metindosyalari)
+
